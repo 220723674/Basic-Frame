ecfc3f896f0efb73a8b029fbf7efe2313149602c
220723674 <140264699+220723674@users.noreply.github.com>
2023-07-22 17:59:22 +0200

    Add files via upload

---
+++ MyFirstFrame.java
@@ -0,0 +1,36 @@
+
+package za.ac.tut.ui;
+
+import javax.swing.JFrame;
+
+
+public class MyFirstFrame extends JFrame{
+
+    public MyFirstFrame(){
+    
+     setTitle("My First GUI");
+     setSize(200,250);
+     setDefaultLookAndFeelDecorated(true);
+     setResizable(true);
+    // setDefaultCoseOperation(EXIT_ON_CLOSE);
+     //setForeground(Color.BLUE);
+     setVisible(true);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+    
+    
+    
+}
